d114e0407905b7523920c1d682bb41bec29ab4d7
mlghr <michael.rosenbaum1@gmail.com>
2020-10-05 13:29:56 -0700

    solution

--- models/customer.js
+++ models/customer.js
@@ -59,6 +59,12 @@ class Customer {
     return await Reservation.getReservationsForCustomer(this.id);
   }
 
+  /** returns full name of customer */
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
   /** save this customer. */
 
   async save() {
